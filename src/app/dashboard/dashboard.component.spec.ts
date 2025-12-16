2ddc2eb3f7746d4544ff50c4c84fa9c234234000
Kapil soni <sonikapil929@gmail.com>
2025-12-16 17:11:32 +0530

    Fixed test case error

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,14 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { SearchPipe } from '../search.pipe';
+import { FormsModule } from '@angular/forms';
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
+      declarations: [DashboardComponent,SearchPipe],
+      imports: [HttpClientModule,FormsModule]
     })
     .compileComponents();
     
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,30 +9,13 @@ import { OidcSecurityService } from 'angular-auth-oidc-client';
 export class DashboardComponent {
 items :any = ['Apple', 'Banana', 'Mango'];
 searchText:string= '';
-   private readonly oidcSecurityService = inject(OidcSecurityService);
-  configuration$ = this.oidcSecurityService.getConfiguration();
-  userData$ = this.oidcSecurityService.userData$;
-isAuthenticated = false;
 
   ngOnInit(): void {
     
 }
 
 
-ngAfterViewInit(): void {
-   this.oidcSecurityService.isAuthenticated$.subscribe(
-      ({ isAuthenticated }) => {
-        this.isAuthenticated = isAuthenticated;
-      }
-    );
-  }
-
 addItem() {
   this.items.push('New Item');
 }
-
- login(): void {
-    this.oidcSecurityService.authorize()
-  }
-
 }
